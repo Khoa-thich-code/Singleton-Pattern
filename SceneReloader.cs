80bc76b529439313f00192a9bbc80c02934970ce
Khoa <130851064+Khoa-thich-code@users.noreply.github.com>
2024-01-02 20:42:39 +0700

    Update SceneReloader.cs

--- SceneReloader.cs
+++ SceneReloader.cs
@@ -1,18 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneReloader : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ReloadScene();
+            Debug.Log("Reload Successfull!");
+        }
+    }
+
+    void ReloadScene()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currentScene.buildIndex);
     }
 }
